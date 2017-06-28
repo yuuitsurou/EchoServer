08ad4571b6bcac1f2d4ae015b760e025ad513d6e
Yuuitsurou Mine <yuuitsurou.mine@gmail.com>
2017-06-28 12:52:59 +0000

    skkdic read

--- CoreEcho/TcpHelper.cs
+++ CoreEcho/TcpHelper.cs
@@ -2,6 +2,8 @@ using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.IO;
+using System.Collections.Generic;
 
 namespace CoreEcho
 {
@@ -10,6 +12,10 @@ namespace CoreEcho
         private static TcpListener listener { get; set; }  
         private static bool accept { get; set; } = false;  
    
+        private static List<String> dic { get; set; }
+
+        private static List<String> midasi { get; set; }
+
         public static void StartServer(int port) 
         {  
             IPAddress address = IPAddress.Parse("127.0.0.1");  
@@ -18,8 +24,20 @@ namespace CoreEcho
             listener.Start();  
             accept = true;  
    
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            String [] ls = File.ReadAllLines("/usr/share/skk/SKK-JISYO.L", Encoding.GetEncoding("EUC-JP"));
+            if (ls != null)
+            {
+                dic = new List<string>(ls);
+                dic.Sort();
+                 midasi = new List<String>();
+                foreach (String i in dic)
+                {
+                    midasi.Add(i.Split(' ')[0]);
+                }
+            }
             Console.WriteLine($"Server started. Listening to TCP clients at 127.0.0.1:{port}");  
-        }  
+       }  
    
         public static void Listen()
        {  
@@ -42,12 +60,21 @@ namespace CoreEcho
                         {  
                             byte[] data = Encoding.ASCII.GetBytes("Send next data: [enter 'quit' to terminate] ");  
                             client.GetStream().Write(data, 0, data.Length);  
-   
+
                             byte[] buffer = new byte[1024];  
-                            client.GetStream().Read(buffer, 0, buffer.Length);  
+                            client.GetStream().Read(buffer, 0, buffer.Length);
    
-                            message = Encoding.ASCII.GetString(buffer);  
-                            Console.WriteLine(message);  
+                            // message = Encoding.ASCII.GetString(buffer);
+                            message = Encoding.UTF8.GetString(buffer);
+                            int index = midasi.BinarySearch(message);
+                            if (index > -1)
+                            {
+                                Console.WriteLine(dic[index].Split(' ')[1]);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Not found...");
+                            }
                         }  
                         Console.WriteLine("Closing connection.");  
                         client.GetStream().Dispose();  
