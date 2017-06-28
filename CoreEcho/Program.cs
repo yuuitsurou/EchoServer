8e4f9da648ad554d7e38dcee4506b147e875b550
Yuuitsurou Mine <ymine@dc4.so-net.ne.jp>
2017-06-28 16:21:09 +0900

    CoreEcho = EchoServer w .Net Core

---
+++ CoreEcho/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CoreEcho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpHelper.StartServer(5678);
+            TcpHelper.Listen();
+        }
+    }
+}
